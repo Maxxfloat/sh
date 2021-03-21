b0205e400be15c674db549ad9da68f6fe4315aa2
Omid-Neshati <65368035+Maxxfloat@users.noreply.github.com>
2021-03-21 22:25:55 +0330

    working on SIM search form

---
+++ src/Component/MainComponents/SearchForm/SearchData.js
@@ -0,0 +1,25 @@
+const num = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 90, 91, 92, 93];
+
+const preNumber = [];
+
+num.map((item) => preNumber.push("9" + item));
+
+export { preNumber };
+
+/* <option value="">فرقی نمی کند</option>
+						<option data-category="1" value="910">0910</option>
+						<option data-category="1" value="911">0911</option>
+						<option data-category="1" value="912">0912</option>
+						<option data-category="1" value="913">0913</option>
+						<option data-category="1" value="914">0914</option>
+						<option data-category="1" value="915">0915</option>
+						<option data-category="1" selected="selected" value="916">
+							0916
+						</option>
+						<option data-category="1" value="917">0917</option>
+						<option data-category="1" value="918">0918</option>
+						<option data-category="1" value="919">0919</option>
+						<option data-category="1" value="990">0990</option>
+						<option data-category="1" value="991">0991</option>
+						<option data-category="1" value="993">0993</option>
+						<option data-category="1" value="992">0992</option> */
--- src/Component/MainComponents/SearchForm/SearchForm.jsx
+++ src/Component/MainComponents/SearchForm/SearchForm.jsx
@@ -1,19 +1,42 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+// import { useForm } from "react-hook-form";
 
 import s from "./SearchForm.module.scss";
 
+import { preNumber } from "./SearchData";
+
 const SearchForm = () => {
+	const numField = [];
+	for (let c = 0; c <= 7; c++) {
+		numField.push(<input />);
+	}
+	// const numFieldDiv = (
+	// 	<div>
+	// 		{numField.map((field) => {
+	// 			return field;
+	// 		})}
+	// 	</div>
+	// );
+
+	console.log(preNumber);
 	return (
 		<div className={s.container}>
 			<form>
-				<label>اوپراتون</label>
-				<select>
-					<option value="hamrahaval">همراه اول</option>
-					<option value="irancel">ایرانسل</option>
-					<option value="ritel">رایتل</option>
-				</select>
-				<input />
+				<div className={s.number_line}>
+					<select value="0916">
+						<option value="">انتخاب نمی کنم</option>
+						{preNumber.map((item) => {
+							return (
+								<option key={item} value={item}>
+									{"0" + item}
+								</option>
+							);
+						})}
+					</select>
+					{numField.map((item, index) => {
+						return <React.Fragment key={index}>{item}</React.Fragment>;
+					})}
+				</div>
 			</form>
 		</div>
 	);
