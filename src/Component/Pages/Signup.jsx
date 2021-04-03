9b423fce3137ffa926f76d20f8008780c8f8d2b0
Omid Neshati <tchandnofame@gmail.com>
2021-04-03 21:28:37 +0430

    username and password field added to signup form

--- src/Component/Pages/Signup.jsx
+++ src/Component/Pages/Signup.jsx
@@ -6,9 +6,9 @@ const Signup = () => {
 	return (
 		<div>
 			<form onSubmit={handleSubmit((data) => console.log(data))}>
-				<label>نام</label>
+				<label>نام و نام خانوادگی</label>
 				<input
-					name="reg.firstName"
+					name="reg.name"
 					ref={register({
 						required: "لطفا نام خودرا وارد",
 						pattern: {
@@ -17,14 +17,35 @@ const Signup = () => {
 						},
 					})}
 				/>
-				<label>نام خانوادگی</label>
+				<label>نام کاربری</label>
 				<input
-					name="reg.LastName"
+					name="reg.userName"
 					ref={register({
-						required: "لطفا نام خانوادگی خودرا وارد",
+						required: "لطفا یک نام کاربری وارد کنید",
 						pattern: {
-							value: /^[آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی]+$/,
-							message: "لطفا زبان فارسی استفاده کنید",
+							value: /^[A-Za-z][A-Za-z0-9]*$/,
+							message: "لطفا فقط از حروف و اعداد انگلیسی استفاده کنید",
+						},
+					})}
+				/>
+				<label>ایمیل</label>
+				<input
+					type="email"
+					name="reg.emailCall"
+					ref={register({
+						required: "لطفا ایمیل خودرا وارد کنید",
+					})}
+				/>
+				<label>رمز عبور</label>
+				<input
+					type="password"
+					name="reg.password"
+					ref={register({
+						required: "لطفا یک رمز عبور دلخواهتان را وارد کنید",
+						pattern: {
+							value: /(?=^.{8,}$)(?=.*\d)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/i,
+							message:
+								"رمز عبور باید دارای حداقل ۸ کاراکتر،حداقل یک حرف بزرگ،حداقل یک حرف کوچک،حداقل یک عدد انگلیسی باشد",
 						},
 					})}
 				/>
