85c33e531b0d2a4b9a257c813d87e4400c4ce036
Omid Neshati <tchandnofame@gmail.com>
2021-04-04 13:35:39 +0430

    home page card position fixed

--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -8,29 +8,21 @@ const Home = () => {
 	return (
 		<div>
 			<div className={s.banner}>
-				<div className={s.cardContainer}>
-					<div className={s.cardSim}>
-						<div>
-							<h1>سیم کارت</h1>
-							<h1>خرید و فروش مستقیم بدون هیچگونه واسطه</h1>
-							<p>بدون هزینه</p>
+				<div className={s.container}>
+					<div className={s.cardContainer}>
+						<div className={s.cardSim}>
+							<div>
+								<h1>سیم کارت</h1>
+								<h1>خرید و فروش مستقیم بدون هیچگونه واسطه</h1>
+								<p>بدون هزینه</p>
+							</div>
 						</div>
-					</div>
-					<div className={s.cardDomain}>
-						<div>
-							<h1>دامنه</h1>
-							<h2>خرید و فروش دامنه</h2>
-							<h2>صدها دامنه آماده انتخاب</h2>
-						</div>
-					</div>
-					<div className={s.cardInfo}>
-						<div>
-							<h1>خرید و فروش بدون واسطه و بصورت مستقیم</h1>
-							<button>
-								<Link className={s.link} to="/signup">
-									ثبت نام
-								</Link>
-							</button>
+						<div className={s.cardDomain}>
+							<div>
+								<h1>دامنه</h1>
+								<h2>خرید و فروش دامنه</h2>
+								<h2>صدها دامنه آماده انتخاب</h2>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -58,7 +50,9 @@ const Home = () => {
 				</div>
 			</div>
 			<div className={s.vitrine}>
-				<Vitrine />
+				<div className={s.vitrineContainer}>
+					<Vitrine />
+				</div>
 			</div>
 		</div>
 	);
--- src/Component/Pages/Signup.jsx
+++ src/Component/Pages/Signup.jsx
@@ -1,55 +1,71 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+import s from "./Signup.module.scss";
+
 const Signup = () => {
-	const { register, handleSubmit } = useForm();
+	const { register, handleSubmit, watch, errors } = useForm();
+	const password = watch("password", "");
 	return (
-		<div>
-			<form onSubmit={handleSubmit((data) => console.log(data))}>
-				<label>نام و نام خانوادگی</label>
-				<input
-					name="reg.name"
-					ref={register({
-						required: "لطفا نام خودرا وارد",
-						pattern: {
-							value: /^[آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی]+$/,
-							message: "لطفا زبان فارسی استفاده کنید",
-						},
-					})}
-				/>
-				<label>نام کاربری</label>
-				<input
-					name="reg.userName"
-					ref={register({
-						required: "لطفا یک نام کاربری وارد کنید",
-						pattern: {
-							value: /^[A-Za-z][A-Za-z0-9]*$/,
-							message: "لطفا فقط از حروف و اعداد انگلیسی استفاده کنید",
-						},
-					})}
-				/>
-				<label>ایمیل</label>
-				<input
-					type="email"
-					name="reg.emailCall"
-					ref={register({
-						required: "لطفا ایمیل خودرا وارد کنید",
-					})}
-				/>
-				<label>رمز عبور</label>
-				<input
-					type="password"
-					name="reg.password"
-					ref={register({
-						required: "لطفا یک رمز عبور دلخواهتان را وارد کنید",
-						pattern: {
-							value: /(?=^.{8,}$)(?=.*\d)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/i,
-							message:
-								"رمز عبور باید دارای حداقل ۸ کاراکتر،حداقل یک حرف بزرگ،حداقل یک حرف کوچک،حداقل یک عدد انگلیسی باشد",
-						},
-					})}
-				/>
-			</form>
+		<div className={s.page}>
+			<div className={s.section}>
+				<form onSubmit={handleSubmit((data) => console.log(data))}>
+					<label>نام و نام خانوادگی</label>
+					<input
+						name="reg.name"
+						ref={register({
+							required: "لطفا نام خودرا وارد",
+							pattern: {
+								value: /^[آابپتثجچحخدذرزژسشصضطظعغفقکگلمنوهی]+$/,
+								message: "لطفا زبان فارسی استفاده کنید",
+							},
+						})}
+					/>
+					<label>نام کاربری</label>
+					<input
+						name="reg.userName"
+						ref={register({
+							required: "لطفا یک نام کاربری وارد کنید",
+							pattern: {
+								value: /^[A-Za-z][A-Za-z0-9]*$/,
+								message: "لطفا فقط از حروف و اعداد انگلیسی استفاده کنید",
+							},
+						})}
+					/>
+					<label>ایمیل</label>
+					<input
+						type="email"
+						name="reg.emailCall"
+						ref={register({
+							required: "لطفا ایمیل خودرا وارد کنید",
+						})}
+					/>
+					<label>رمز عبور</label>
+					<input
+						type="password"
+						name="reg.password"
+						ref={register({
+							required: "لطفا یک رمز عبور دلخواهتان را وارد کنید",
+							pattern: {
+								value: /(?=^.{8,}$)(?=.*\d)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/i,
+								message:
+									"رمز عبور باید دارای حداقل ۸ کاراکتر،حداقل یک حرف بزرگ،حداقل یک حرف کوچک،حداقل یک عدد انگلیسی باشد",
+							},
+						})}
+					/>
+					{errors.password && <p>{errors.password.message}</p>}
+					<input
+						type="password"
+						name="reg.passwordConfirm"
+						ref={register({
+							required: "لطفا عبور خودرا تکرار کنید",
+							validate: (value) =>
+								value === password || "تکرار رمز عبور اشتباه است",
+						})}
+					/>
+					{errors.passwordConfirm && <p>{errors.passwordConfirm.message}</p>}
+				</form>
+			</div>
 		</div>
 	);
 };
